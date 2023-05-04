973c3762599ddc33ec80b50d58f78bdef5bc0502
akt0001c <110126989+akt0001c@users.noreply.github.com>
2023-05-04 21:32:13 +0530

    'fp06_253day2'

--- OnlineVegetableStore/src/main/java/com/example/demo/repositories/CustomerDao.java
+++ OnlineVegetableStore/src/main/java/com/example/demo/repositories/CustomerDao.java
@@ -2,14 +2,15 @@ package com.example.demo.repositories;
 import com.example.demo.entities.*;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerDao extends JpaRepository<Customer,Integer> {
-public Customer findByEmailId(String emailId);
+public Optional<Customer> findByEmailId(String emailId);
+
 public List<Customer> findByName(String name);
-public Customer findByMobileNumber(String mobileNumber);
+public Optional<Customer> findByMobileNumber(String mobileNumber);
 }
--- OnlineVegetableStore/src/main/java/com/example/demo/services/CustomerServiceImpl.java
+++ OnlineVegetableStore/src/main/java/com/example/demo/services/CustomerServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.demo.services;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entities.Admin;
 import com.example.demo.entities.Customer;
+import com.example.demo.exceptions.AdminException;
 import com.example.demo.exceptions.CustomerException;
 import com.example.demo.exceptions.CustomerNotFoundException;
 import com.example.demo.repositories.CustomerDao;
@@ -18,7 +20,7 @@ public class CustomerServiceImpl implements CustomerService {
 
 	
 	/**
-	 * This method is used to add Customer details in database
+	 * This method is used to add Customer details in  the database
 	 * @paramCustomer 
 	 * @return Customer
 	 * @exception CustomerException
@@ -26,9 +28,8 @@ public class CustomerServiceImpl implements CustomerService {
 	 */
 	@Override
 	public Customer addCustomer(Customer customer) {
-		Customer obCustomer= customerDao.findByEmailId(customer.getEmailId());
-		    if(obCustomer!=null)
-		    	  throw new CustomerException("Provided email id  is already exist so Please try with another email id");
+		Customer obCustomer= customerDao.findByEmailId(customer.getEmailId()).orElseThrow(()->new CustomerException("Provided email id  is already exist so Please try with another email id"));
+		   
 		 obCustomer= customerDao.save(customer);
 		return obCustomer;
 	}
@@ -36,9 +37,20 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public Customer updateCustomer(Customer customer) {
 		
-		
+		Customer res= customerDao.findByEmailId(customer.getEmailId()).orElseThrow(()->new CustomerException("Customer details Not found"));
+	   
+	    
+	    if(!customer.getEmailId().equals(""))
+		{
+			res.setEmailId(customer.getEmailId());
+			
+		}
+	   
+	   
+	    customerDao.save(res);
+	   
 			    
-		return null;
+		return res;
 	}
 
 	
@@ -51,9 +63,8 @@ public class CustomerServiceImpl implements CustomerService {
 	 */
 	@Override
 	public Customer removeCustomer(Customer customer) {
-		Customer res= customerDao.findByEmailId(customer.getEmailId());
-		if(res==null)
-			  throw new CustomerNotFoundException("Provided  customer details are invalid ,Please try again with correct details");
+		Customer res= customerDao.findByEmailId(customer.getEmailId()).orElseThrow(()->new CustomerNotFoundException("Provided  customer details are invalid ,Please try again with correct details"));
+		
 		
 		 customerDao.delete(res);
 		 return res;
@@ -63,7 +74,24 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public Customer viewCustomer(Customer customer) {
 		
-		return null;
+		
+		if(!customer.getEmailId().equals(""))
+		{
+			Customer ans = customerDao.findByEmailId(customer.getEmailId()).orElseThrow(()->new CustomerException("Invalid details"));
+			return ans;
+		}
+		
+		if(!customer.getMobileNumber().equals(""))
+		{
+			Customer ans = customerDao.findByMobileNumber(customer.getMobileNumber()).orElseThrow(()-> new CustomerException("Invalid details"));
+			return ans;
+		}
+		else {
+			 throw new AdminException("Details are empty so Admin details cannot be found");
+		}
+		
+		
+		
 	}
 
 	/**
