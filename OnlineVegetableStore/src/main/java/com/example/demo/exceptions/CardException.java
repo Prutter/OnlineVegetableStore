d8ff58a4ecdf3068172933dc2643c89bdcfeb134
Kashish <kashishbansal044@gmail.com>
2023-05-06 23:52:10 +0530

    day05 fw21_0546

---
+++ OnlineVegetableStore/src/main/java/com/example/demo/exceptions/CardException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exceptions;
+
+public class CardException extends Exception{
+	public CardException(String msg) {
+		super(msg);
+	}
+}
---
+++ OnlineVegetableStore/src/main/java/com/example/demo/services/CardServices.java
@@ -0,0 +1,14 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.CardWallet;
+import com.example.demo.exceptions.CardException;
+import com.example.demo.exceptions.CustomerException;
+
+public interface CardServices {
+	
+	public CardWallet addCard(CardWallet cardwallet, Integer cid) throws CustomerException;
+	
+	public Integer AddAmountToWallet(CardWallet cardWallet, Integer cid, Integer amount) throws CardException;
+	
+	public CardWallet getCardDetails(Integer cid) throws CardException;
+}
