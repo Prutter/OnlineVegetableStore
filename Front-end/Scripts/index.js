52d0dc95304457906c196cfbffb42fa64cc23b6f
akt0001c <110126989+akt0001c@users.noreply.github.com>
2023-05-10 08:39:00 +0530

    update

--- Front-end/Scripts/index.js
+++ Front-end/Scripts/index.js
@@ -1,5 +1,7 @@
 let addressbtn= document.querySelector("#address-page form");
 let addressbarEnableButtton= document.querySelector("#address-btn");
+let cardEnableButton= document.querySelector("#card-btn");
+let cardbtn= document.querySelector("#card-page form");
 
 window.onload = ()=>{
     let customerdetails= JSON.parse(localStorage.getItem("customer"))|| undefined;
@@ -13,6 +15,7 @@ window.onload = ()=>{
         document.querySelector("#logout-div").style.display= "block";
         document.querySelector("#cart-div").style.pointerEvents="all";
         document.querySelector("#address-btn").style.display="block";
+        document.querySelector("#card-btn").style.display="block";
       
       }
        else 
@@ -24,52 +27,80 @@ window.onload = ()=>{
         document.querySelector("#logout-div").style.display="none";
         document.querySelector("#cart-div").style.pointerEvents="none";
         document.querySelector("#address-btn").style.display="none";
+        document.querySelector("#card-btn").style.display="none";
 
          }
 
-
-    document.querySelector("#logout-div").onclick = (e)=>{
-       e.preventDefault();
-       localStorage.setItem("loggedsession",JSON.stringify(false));
-       window.location.href="index.html"
-    };
+};
 
 
-    addressbarEnableButtton.onclick= ()=>{
-      document.querySelector("#address-page").style.display="flex";
-      document.querySelector("#main").style.display="none";
-  };
+document.querySelector("#logout-div").onclick = (e)=>{
+  e.preventDefault();
+  localStorage.setItem("loggedsession",JSON.stringify(false));
+  window.location.href="index.html"
+};
+
+cardEnableButton.onclick= ()=>{
+ document.querySelector("#card-page").style.display="flex";
+ document.querySelector("#main").style.display="none";
+};
 
 
-    addressbtn.onsubmit=(e)=>{
-      e.preventDefault();
-      let flatno= document.querySelector("#flatno");
-      let bname= document.querySelector("#bname");
-      let area= document.querySelector("#area");
-      let city= document.querySelector("#city");
-      let state= document.querySelector("#state");
-      let pcode= document.querySelector("#pcode");
+addressbarEnableButtton.onclick= ()=>{
+ document.querySelector("#address-page").style.display="flex";
+ document.querySelector("#main").style.display="none";
+};
+
+
+addressbtn.onsubmit=(e)=>{
+  e.preventDefault();
+  let flatno= document.querySelector("#flatno");
+  let bname= document.querySelector("#bname");
+  let area= document.querySelector("#area");
+  let city= document.querySelector("#city");
+  let state= document.querySelector("#state");
+  let pcode= document.querySelector("#pcode");
+
+ let addressDetails={
+      "area" : area.value,
+      "state" :state.value,
+      "pincode" : pcode.value,
+      "city" :city.value,
+      "buildingName" : bname.value,
+      "flatNo" :flatno.value
+  }
+
+  bname.value="";
+  flatno.value="";
+  area.value="";
+  city.value="";
+  state.value="";
+  pcode.value="";
+
+  document.querySelector("#address-page").style.display="none";
+  document.querySelector("#main").style.display="block";
   
-     let addressDetails={
-          "area" : area.value,
-          "state" :state.value,
-          "pincode" : pcode.value,
-          "city" :city.value,
-          "buildingName" : bname.value,
-          "flatNo" :flatno.value
-      }
-  
-      bname.value="";
-      flatno.value="";
-      area.value="";
-      city.value="";
-      state.value="";
-      pcode.value="";
-  
-      document.querySelector("#address-page").style.display="none";
-      document.querySelector(".main").style.display="block";
-      document.querySelector("#submit-btn").disabled=false;
-  
-  };
 
 };
+
+cardbtn.onsubmit= (e)=>{
+  e.preventDefault();
+   let cardNo= document.querySelector("#cno");
+   let expDate= document.querySelector("#exp-date");
+   let walletAmount=document.querySelector("#wamount"); 
+
+   let cardDetails={
+    "cardNo" : cardNo.value,
+    "expiryDate" :expDate.value,
+    "walletAmount" : walletAmount.value
+   }
+
+   cardNo.value="";
+   expDate.value="";
+   walletAmount.value="";
+
+  document.querySelector("#card-page").style.display="none";
+  document.querySelector("#main").style.display="block";
+};
+
+
